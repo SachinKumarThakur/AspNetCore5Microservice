5a9f2a5817a7eab19ea465dbcc13a0113c3287b4
Sachin Kumar <sachinengineer@hotmail.com>
2021-07-08 23:26:58 +0530

    Basket API commited

---
+++ src/Services/Basket/Basket.API/Repositories/Interfaces/IBasketRepository.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace Basket.API.Repositories.Interfaces
+{
+    using Basket.API.Entities;
+    using System.Threading.Tasks;
+    public interface IBasketRepository
+    {
+        Task<ShoppingCart> GetBasket(string userName);
+        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task DeleteBasket(string userName);
+    }
+}
